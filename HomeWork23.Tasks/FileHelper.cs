6e8fdebfea37208a90dbaed7f8e149565adbc62b
Mike <m.a.ilnitsky@yandex.ru>
2024-09-05 02:34:58 +0700

    Added home work "Tasks" for lection #23

---
+++ HomeWork23.Tasks/FileHelper.cs
@@ -0,0 +1,138 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork23.Tasks;
+
+public static class FileHelper
+{
+    public static int GetFileSymbolsCount(string filePath)
+    {
+        using var reader = new StreamReader(filePath);
+
+        var symbolsCount = 0;
+
+        while (reader.Read() > -1)
+        {
+            symbolsCount++;
+        }
+
+        return symbolsCount;
+    }
+
+    public static int GetFileSymbolsCount(string filePath, char symbol)
+    {
+        using var reader = new StreamReader(filePath);
+
+        var symbolsCount = 0;
+        int symbolNumber;
+
+        while ((symbolNumber = reader.Read()) > -1)
+        {
+            if (symbol == (char)symbolNumber)
+            {
+                symbolsCount++;
+            }
+        }
+
+        return symbolsCount;
+    }
+
+    public static int GetDirectoryFilesSymbolsCount(string directoryPath)
+    {
+        var files = Directory.EnumerateFiles(directoryPath).ToArray();
+        var tasks = new Task<int>[files.Length];
+
+        var i = 0;
+
+        foreach (var filePath in files)
+        {
+            tasks[i] = new Task<int>(() => GetFileSymbolsCount(filePath));
+            tasks[i].Start();
+            i++;
+        }
+
+        Task.WaitAll(tasks);
+        int sumCount = 0;
+
+        foreach (var task in tasks)
+        {
+            sumCount += task.Result;
+        }
+
+        return sumCount;
+    }
+
+    public static int GetDirectoryFilesSymbolsCount(string directoryPath, char symbol)
+    {
+        var files = Directory.EnumerateFiles(directoryPath).ToArray();
+        var tasks = new Task<int>[files.Length];
+
+        var i = 0;
+
+        foreach (var filePath in files)
+        {
+            tasks[i] = new Task<int>(() => GetFileSymbolsCount(filePath, symbol));
+            tasks[i].Start();
+            i++;
+        }
+
+        Task.WaitAll(tasks);
+        int sumCount = 0;
+
+        foreach (var task in tasks)
+        {
+            sumCount += task.Result;
+        }
+
+        return sumCount;
+    }
+
+    public static async Task<int> GetDirectoryFilesSymbolsCountAsync(string directoryPath)
+    {
+        var files = Directory.EnumerateFiles(directoryPath).ToArray();
+        var tasks = new Task<int>[files.Length];
+
+        var i = 0;
+
+        foreach (var filePath in files)
+        {
+            tasks[i] = Task.Run(() => GetFileSymbolsCount(filePath));
+            i++;
+        }
+
+        await Task.WhenAll(tasks);
+        int sumCount = 0;
+
+        foreach (var task in tasks)
+        {
+            sumCount += task.Result;
+        }
+
+        return sumCount;
+    }
+
+    public static async Task<int> GetDirectoryFilesSymbolsCountAsync(string directoryPath, char symbol)
+    {
+        var files = Directory.EnumerateFiles(directoryPath).ToArray();
+        var tasks = new Task<int>[files.Length];
+
+        var i = 0;
+
+        foreach (var filePath in files)
+        {
+            tasks[i] = Task.Run(() => GetFileSymbolsCount(filePath, symbol));
+            i++;
+        }
+
+        await Task.WhenAll(tasks);
+        int sumCount = 0;
+
+        foreach (var task in tasks)
+        {
+            sumCount += task.Result;
+        }
+
+        return sumCount;
+    }
+}
---
+++ HomeWork23.Tasks/Program.cs
@@ -0,0 +1,160 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HomeWork23.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+static void ReadAndCount(string filePath, int fileNumber)
+{
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    var fileInfo = new FileInfo(filePath);
+    var allSymbolsCount = FileHelper.GetFileSymbolsCount(filePath);
+    var spaceSymbolsCount = FileHelper.GetFileSymbolsCount(filePath, ' ');
+    stopwatch.Stop();
+    Console.WriteLine($"[{fileNumber,2}] {fileInfo.Name,-30} Всего символов: {allSymbolsCount,5} \tПробелов: {spaceSymbolsCount,5} \tза {stopwatch.Elapsed.TotalMicroseconds,8:f1} микросекунд");
+}
+
+static void WriteResult(int filesCount, Stopwatch stopwatch)
+{
+    Console.WriteLine($"Прочитано {filesCount} файла за {stopwatch.Elapsed.TotalMicroseconds} микросекунд");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+static void TestSequentialReading(IList<string> files)
+{
+    var stopwatch = new Stopwatch();
+    var number = 1;
+
+    Console.WriteLine("Последовательное чтение файлов:");
+
+    stopwatch.Start();
+
+    foreach (var filePath in files)
+    {
+        ReadAndCount(filePath, number);
+        number++;
+    }
+
+    stopwatch.Stop();
+
+    WriteResult(files.Count, stopwatch);
+}
+
+static void TestParallelReadingWithFor(IList<string> files)
+{
+    var stopwatch = new Stopwatch();
+
+    Console.WriteLine("Параллельное чтение файлов с помощью Parallel.For:");
+
+    stopwatch.Start();
+    var parallelLoopResult = Parallel.For(0, files.Count, i => ReadAndCount(files[i], i + 1));
+    stopwatch.Stop();
+
+    WriteResult(files.Count, stopwatch);
+}
+
+static void TestParallelReadingWithTaskWaitAll(IList<string> files)
+{
+    var stopwatch = new Stopwatch();
+    var tasks = new Task[files.Count];
+    var number = 0;
+
+    Console.WriteLine("Параллельное чтение файлов с помощью Task и WaitAll:");
+
+    stopwatch.Start();
+
+    foreach (var filePath in files)
+    {
+        var currentIndex = number;
+        number++;
+        var currentNumber = number;
+
+        tasks[currentIndex] = Task.Run(() => ReadAndCount(filePath, currentNumber));
+    }
+
+    Task.WaitAll(tasks);
+    stopwatch.Stop();
+
+    WriteResult(files.Count, stopwatch);
+}
+
+static void TestParallelCountingWithWaitAll(string directoryPath)
+{
+    var stopwatch = new Stopwatch();
+
+    Console.WriteLine("WaitAll: Параллельный подсчёт всех символов и пробелов во всех файлах папки");
+
+    stopwatch.Start();
+    var allSymbolsCount = FileHelper.GetDirectoryFilesSymbolsCount(directoryPath);
+    stopwatch.Stop();
+    var allSymbolsMicroseconds = stopwatch.Elapsed.TotalMicroseconds;
+
+    stopwatch.Restart();
+    var spaceSymbolsCount = FileHelper.GetDirectoryFilesSymbolsCount(directoryPath, ' ');
+    stopwatch.Stop();
+    var spaceSymbolsMicroseconds = stopwatch.Elapsed.TotalMicroseconds;
+
+    Console.WriteLine($"Всего символов в файлах: {allSymbolsCount,6}, подсчитано за {allSymbolsMicroseconds,8:f1} микросекунд");
+    Console.WriteLine($"Всего пробелов в файлах: {spaceSymbolsCount,6}, подсчитано за {spaceSymbolsMicroseconds,8:f1} микросекунд");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+static async Task TestParallelCountingWithWhenAll(string directoryPath)
+{
+    var stopwatch = new Stopwatch();
+
+    Console.WriteLine("WhenAll: Параллельный подсчёт всех символов и пробелов во всех файлах папки");
+
+    stopwatch.Start();
+    var allSymbolsCount = await FileHelper.GetDirectoryFilesSymbolsCountAsync(directoryPath);
+    stopwatch.Stop();
+    var allSymbolsMicroseconds = stopwatch.Elapsed.TotalMicroseconds;
+
+    stopwatch.Restart();
+    var spaceSymbolsCount = await FileHelper.GetDirectoryFilesSymbolsCountAsync(directoryPath, ' ');
+    stopwatch.Stop();
+    var spaceSymbolsMicroseconds = stopwatch.Elapsed.TotalMicroseconds;
+
+    Console.WriteLine($"Всего символов в файлах: {allSymbolsCount,6}, подсчитано за {allSymbolsMicroseconds,8:f1} микросекунд");
+    Console.WriteLine($"Всего пробелов в файлах: {spaceSymbolsCount,6}, подсчитано за {spaceSymbolsMicroseconds,8:f1} микросекунд");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+var currentDirectoryPath = Environment.CurrentDirectory;
+var selectedDirectoryPath = new DirectoryInfo(currentDirectoryPath)
+        .Parent
+        ?.Parent
+        ?.Parent
+        ?.FullName;
+
+if (string.IsNullOrWhiteSpace(selectedDirectoryPath))
+{
+    Console.WriteLine("Директория не выбрана!");
+    return;
+}
+
+Console.WriteLine($"Выбранная директория: {selectedDirectoryPath}");
+Console.WriteLine();
+
+var files = Directory.EnumerateFiles(selectedDirectoryPath).ToList();
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("==============");
+    Console.WriteLine($"{i,2}-й проход");
+    Console.WriteLine();
+
+    TestSequentialReading(files);
+    TestParallelReadingWithFor(files);
+    TestParallelReadingWithTaskWaitAll(files);
+
+    TestParallelCountingWithWaitAll(selectedDirectoryPath);
+    await TestParallelCountingWithWhenAll(selectedDirectoryPath);
+}
